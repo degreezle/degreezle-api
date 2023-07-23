b520ce7cdbeba675403fdb5eaf4f2d06d3093424
Andrew <andrewtothemax@gmail.com>
2023-07-23 20:54:03 +1200

    Allow nulls

--- api/migrations/0004_puzzle_author.py
+++ api/migrations/0004_puzzle_author.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='puzzle',
             name='author',
-            field=models.CharField(blank=True, max_length=64),
+            field=models.CharField(blank=True, max_length=64, null=True),
         ),
     ]
